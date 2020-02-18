47207144413875cc3db0f6ea0fc18120b3c11ee2
corrado4eyes <corrado@splendo.com>
2020-02-18 14:42:06 +0100

    [#4] Added adapter

---
+++ app/src/main/java/com/corrado4eyes/dehet/ui/adapters/HistoryAdapter.kt
@@ -0,0 +1,44 @@
+package com.corrado4eyes.dehet.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.corrado4eyes.dehet.databinding.HistoryEntryBinding
+import com.corrado4eyes.dehet.models.HistoryEntry
+import com.corrado4eyes.dehet.ui.viewModels.HistoryEntryViewModel
+
+class HistoryAdapter: RecyclerView.Adapter<HistoryAdapter.EntryHolder>() {
+
+    var historyList: List<HistoryEntry> = listOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntryHolder {
+        val view = HistoryEntryBinding.inflate(LayoutInflater.from(parent.context),
+            parent, false)
+        return EntryHolder(view)
+    }
+
+    override fun getItemCount(): Int = historyList.size
+
+    override fun onBindViewHolder(holder: EntryHolder, position: Int) {
+        val entry = historyList[position]
+        return holder.bind(entry)
+    }
+
+    fun updateEntries(newList: List<HistoryEntry>) {
+        historyList = newList
+        notifyDataSetChanged()
+    }
+
+    class EntryHolder(private val view: HistoryEntryBinding): RecyclerView.ViewHolder(view.root) {
+        fun bind(entry: HistoryEntry) {
+            view.viewModel = HistoryEntryViewModel(entry.adverb, entry.article)
+        }
+    }
+}
+
+@BindingAdapter("entriesList")
+fun RecyclerView.bindEntries(newList: List<HistoryEntry>) {
+    (adapter as? HistoryAdapter)?.updateEntries(newList)
+}
