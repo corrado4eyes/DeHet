aca295eff5db7a459f4d3f49bd652175516d596b
corrado4eyes <corrado@splendo.com>
2020-02-19 10:26:03 +0100

    [#8] Substituted webscraping with api call and injected repository

--- app/src/main/java/com/corrado4eyes/dehet/delegates/ViewModelDelegate.kt
+++ app/src/main/java/com/corrado4eyes/dehet/delegates/ViewModelDelegate.kt
@@ -2,17 +2,20 @@ package com.corrado4eyes.dehet.delegates
 
 import android.util.Log
 import com.corrado4eyes.dehet.models.HistoryEntry
+import com.corrado4eyes.dehet.repos.YandexRepository
 import com.corrado4eyes.dehet.util.Constants
 import com.corrado4eyes.dehet.util.doInBackground
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-import java.io.IOException
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-class ViewModelDelegate {
+class ViewModelDelegate: KoinComponent {
     companion object {
         private const val TAG = "ViewModelDelegate"
     }
+
+    private val yandexRepo by inject<YandexRepository>()
 
     private suspend fun createDocument(word: String): Document = doInBackground {
         val url = "${Constants.url}/$word"
@@ -21,18 +24,9 @@ class ViewModelDelegate {
     }
 
     suspend fun getArticle(word: String): HistoryEntry = doInBackground {
-        var adverb: String?
-        var article: String?
-        try {
-            val document = createDocument(word)
-            val articleElement: Element = document.getElementById("content")
-            val wordElement = articleElement.getElementsByTag("h1").first()
-            adverb = wordElement.text().split(" ").last()
-            article = wordElement.getElementsByTag("span").first().text()
-        }catch (e: IOException) {
-            Log.d(TAG, "There is an error ${e.message}")
-            throw(e)
-        }
+        val response = yandexRepo.getTranslation("en-nl", word)
+        val article = response.text.first().split(" ").first()
+        val adverb = response.text.first().split(" ").last()
         return@doInBackground HistoryEntry(article, adverb)
     }
 
