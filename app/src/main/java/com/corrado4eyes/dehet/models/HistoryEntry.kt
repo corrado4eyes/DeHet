e4c221d6ddd631364e76ab1f983e0b926313508b
corrado4eyes <corrado@splendo.com>
2020-02-21 11:11:47 +0100

    [#3] Renamed value

--- app/src/main/java/com/corrado4eyes/dehet/models/HistoryEntry.kt
+++ app/src/main/java/com/corrado4eyes/dehet/models/HistoryEntry.kt
@@ -2,9 +2,9 @@ package com.corrado4eyes.dehet.models
 
 data class HistoryEntry(
     val article: String = "",
-    val adverb: String = "",
+    val word: String = "",
     val isFavourite: Boolean = false) {
     override fun toString(): String {
-        return "${article.capitalize()} $adverb"
+        return "${article.capitalize()} $word"
     }
 }
