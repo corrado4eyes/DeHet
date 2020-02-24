619288df64760a0468adb82038995e4164c53625
corrado4eyes <corrado@splendo.com>
2020-02-24 13:05:41 +0100

    [#13] Added image binding adapter

--- app/src/main/java/com/corrado4eyes/dehet/ui/adapters/HistoryAdapter.kt
+++ app/src/main/java/com/corrado4eyes/dehet/ui/adapters/HistoryAdapter.kt
@@ -2,20 +2,26 @@ package com.corrado4eyes.dehet.ui.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.annotation.DrawableRes
 import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import coil.api.load
+import com.corrado4eyes.dehet.R
 import com.corrado4eyes.dehet.databinding.HistoryEntryBinding
 import com.corrado4eyes.dehet.models.HistoryEntry
 import com.corrado4eyes.dehet.ui.viewModels.HistoryEntryViewModel
+import kotlinx.android.synthetic.main.history_entry.view.*
 
 class HistoryAdapter: RecyclerView.Adapter<HistoryAdapter.EntryHolder>() {
 
     var historyList: List<HistoryEntry> = listOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntryHolder {
-        val view = HistoryEntryBinding.inflate(LayoutInflater.from(parent.context),
-            parent, false)
-        return EntryHolder(view)
+        val binding = DataBindingUtil.inflate<HistoryEntryBinding>(LayoutInflater.from(parent.context),
+            R.layout.history_entry, parent, false)
+        return EntryHolder(binding)
     }
 
     override fun getItemCount(): Int = historyList.size
@@ -30,14 +36,26 @@ class HistoryAdapter: RecyclerView.Adapter<HistoryAdapter.EntryHolder>() {
         notifyDataSetChanged()
     }
 
-    class EntryHolder(private val view: HistoryEntryBinding): RecyclerView.ViewHolder(view.root) {
+    class EntryHolder(private val binding: HistoryEntryBinding):
+        RecyclerView.ViewHolder(binding.root) {
+
         fun bind(entry: HistoryEntry) {
-            view.viewModel = HistoryEntryViewModel(entry)
+            val viewModel = HistoryEntryViewModel(entry)
+            binding.viewModel = viewModel
+            itemView.favouriteButton.setOnClickListener {
+                viewModel.onToggleFavouriteButton()
+            }
         }
+
     }
 }
 
 @BindingAdapter("entriesList")
 fun RecyclerView.bindEntries(newList: List<HistoryEntry>) {
     (adapter as? HistoryAdapter)?.updateEntries(newList)
+}
+
+@BindingAdapter("bind:entryImg")
+fun ImageButton.setImage(@DrawableRes resId: Int) {
+    this.load(resId)
 }
