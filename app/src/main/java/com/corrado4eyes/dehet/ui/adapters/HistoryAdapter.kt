dee16fad76d7b23d18f486212702b294919c1d88
corrado4eyes <corrado@splendo.com>
2020-02-24 14:00:01 +0100

    [#13] Added events onClick on entry

--- app/src/main/java/com/corrado4eyes/dehet/ui/adapters/HistoryAdapter.kt
+++ app/src/main/java/com/corrado4eyes/dehet/ui/adapters/HistoryAdapter.kt
@@ -11,17 +11,16 @@ import coil.api.load
 import com.corrado4eyes.dehet.R
 import com.corrado4eyes.dehet.databinding.HistoryEntryBinding
 import com.corrado4eyes.dehet.models.HistoryEntry
-import com.corrado4eyes.dehet.ui.viewModels.HistoryEntryViewModel
 import kotlinx.android.synthetic.main.history_entry.view.*
 
-class HistoryAdapter: RecyclerView.Adapter<HistoryAdapter.EntryHolder>() {
+class HistoryAdapter(private val entryEvent: HistoryEntryEvent): RecyclerView.Adapter<HistoryAdapter.EntryHolder>() {
 
     var historyList: List<HistoryEntry> = listOf()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EntryHolder {
         val binding = DataBindingUtil.inflate<HistoryEntryBinding>(LayoutInflater.from(parent.context),
             R.layout.history_entry, parent, false)
-        return EntryHolder(binding)
+        return EntryHolder(binding, entryEvent)
     }
 
     override fun getItemCount(): Int = historyList.size
@@ -36,18 +35,36 @@ class HistoryAdapter: RecyclerView.Adapter<HistoryAdapter.EntryHolder>() {
         notifyDataSetChanged()
     }
 
-    class EntryHolder(private val binding: HistoryEntryBinding):
+    class EntryHolder(private val binding: HistoryEntryBinding,
+                      private val entryEvent: HistoryEntryEvent):
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(entry: HistoryEntry) {
-            val viewModel = HistoryEntryViewModel(entry)
-            binding.viewModel = viewModel
+            setListeners()
+            setupUi(entry)
+        }
+
+        private fun setListeners() {
             itemView.favouriteButton.setOnClickListener {
-                viewModel.onToggleFavouriteButton()
+                entryEvent.onFavouriteButtonClicked(adapterPosition)
             }
         }
 
+        private fun setupUi(entry: HistoryEntry) {
+            binding.articleLabel.text = entry.toString()
+            binding.favouriteButton.setImage(
+                when(entry.isFavourite) {
+                    true -> R.drawable.bookmark
+                    else -> R.drawable.empty_bookmark
+                }
+            )
+        }
+
     }
+}
+
+interface HistoryEntryEvent {
+    fun onFavouriteButtonClicked(position: Int)
 }
 
 @BindingAdapter("entriesList")
