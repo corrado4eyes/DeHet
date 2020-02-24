bc81a932c5a41e51306e58ac2fe5a09449dd1541
corrado4eyes <corrado@splendo.com>
2020-02-24 15:11:41 +0100

    [#13] Removed unused imports

--- app/src/main/java/com/corrado4eyes/dehet/util/CoroutineUtil.kt
+++ app/src/main/java/com/corrado4eyes/dehet/util/CoroutineUtil.kt
@@ -1,10 +1,8 @@
 package com.corrado4eyes.dehet.util
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.koin.core.KoinComponent
-import org.koin.core.inject
 
 class CoroutineUtil(private val dispatcher: DispatcherProvider): KoinComponent {
 
