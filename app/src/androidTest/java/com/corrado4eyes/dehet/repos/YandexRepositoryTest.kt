6631d49c4ef4b82c73c7c05a0de2cfb81a5a1974
corrado4eyes <corrado@splendo.com>
2020-02-25 13:51:12 +0100

    [#17] Written tests for  repos

---
+++ app/src/androidTest/java/com/corrado4eyes/dehet/repos/YandexRepositoryTest.kt
@@ -0,0 +1,52 @@
+package com.corrado4eyes.dehet.repos
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.corrado4eyes.dehet.testUtils.CoroutineTestRule
+import com.corrado4eyes.dehet.testUtils.TestModules
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.core.context.GlobalContext
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.test.KoinTest
+import org.koin.test.get
+import org.mockito.MockitoAnnotations
+import kotlin.test.assertEquals
+
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class YandexRepositoryTest: KoinTest {
+
+    private lateinit var yandexRepo: YandexRepository
+
+    @Before
+    fun before() {
+        MockitoAnnotations.initMocks(this)
+        if (GlobalContext.getOrNull() == null) {
+            startKoin {
+                modules(TestModules.modules)
+            }
+        }
+
+        yandexRepo = get()
+    }
+
+    @After
+    fun after() {
+        stopKoin()
+    }
+
+    @Test
+    fun getTranslation() = runBlocking {
+        val result = yandexRepo.getTranslation("en-nl", "the cat")
+        assertEquals("de kat", result.text.first())
+    }
+}
